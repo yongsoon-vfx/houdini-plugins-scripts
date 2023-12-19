705c3217734ff02457602536bb9a4110f6b07f95
yongsoon-vfx <yongg.soonn@gmail.com>
2023-12-19 14:21:51 +0800

    reformat, cleaned up ui

--- batch_export_abc.py
+++ batch_export_abc.py
@@ -1,32 +1,40 @@
-import hou , os
+import hou
+import os
 
-sFrame = hou.expandString("$RFSTART")
-eFrame = hou.expandString("$RFEND")
 
-obj = hou.node("/obj")
-geoNode = obj.createNode('geo','helper')
-objMerge = geoNode.createNode('object_merge','import_geo')
-ropAbc = geoNode.createNode('rop_alembic','out_rop')
-ropAbc.setInput(0,objMerge)
-geoNode.hide(True)
-
-selectedNodes = hou.ui.selectNode(multiple_select=True,node_type_filter=hou.nodeTypeFilter.Sop)
-confirm = hou.ui.displayConfirmation(f"Selected Nodes:  {selectedNodes}")
-if confirm == False:
+def cleanUp():
     geoNode.destroy()
     exit()
 
 
+sFrame = hou.expandString("$RFSTART")
+eFrame = hou.expandString("$RFEND")
+obj = hou.node("/obj")
+geoNode = obj.createNode("geo", "helper")
+objMerge = geoNode.createNode("object_merge", "import_geo")
+ropAbc = geoNode.createNode("rop_alembic", "out_rop")
+ropAbc.setInput(0, objMerge)
+geoNode.hide(True)
+
+selectedNodes = hou.ui.selectNode(
+    multiple_select=True,
+    node_type_filter=hou.nodeTypeFilter.Sop,
+)
+if selectedNodes == None:
+    hou.ui.displayConfirmation("No Nodes Selected",
+                               severity=hou.severityType.Error,
+                               title="Error")
+    cleanUp()
+
+selectedNodesStr = "\n".join(selectedNodes)
+confirm = hou.ui.displayConfirmation(f"Selected Nodes:\n{selectedNodesStr}")
+if confirm == False:
+    cleanUp()
+
 for node in selectedNodes:
     nodeName = os.path.basename(node)
-    objMerge.parm('objpath1').set(node)
-    ropAbc.parm('filename').set(f'$HIP/abc/{nodeName}.abc')
-    ropAbc.render(frame_range=(int(sFrame),int(eFrame)))
+    objMerge.parm("objpath1").set(node)
+    ropAbc.parm("filename").set(f"$HIP/abc/{nodeName}.abc")
+    ropAbc.render(frame_range=(int(sFrame), int(eFrame)))
 
-geoNode.destroy()
-
-
-
-"""
-
-"""
+cleanUp()
