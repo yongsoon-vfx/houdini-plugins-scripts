44e80bf14fd6be219f96b52a108735acfc227d38
yongsoon-vfx <yongg.soonn@gmail.com>
2024-02-18 23:55:47 +0800

    Update node_extended.py

--- node_extended.py
+++ node_extended.py
@@ -20,11 +20,14 @@ def arrange_vertical(nodes, init_pos, spacing):
         init_pos(<hou.Vector2>): Initial position to place the first Node
         spacing(float): Spacing between each Node
 
+    Returns 1 if Successful
+
     """
     type_check_node(nodes)
     for i, node in enumerate(nodes):
         position = init_pos + hou.Vector2(0, i * spacing)
         node.setPosition(position)
+    return 1
 
 
 def arrange_horizontal(nodes, init_pos, spacing):
@@ -35,9 +38,26 @@ def arrange_horizontal(nodes, init_pos, spacing):
         init_pos(<hou.Vector2>): Initial position to place the first Node
         spacing(float): Spacing between each Node
 
+    Returns 1 if Successful
+
     """
     type_check_node(nodes)
     type_check_vector2(init_pos)
     for i, node in enumerate(nodes):
         position = init_pos + hou.Vector2(i * spacing, 0)
         node.setPosition(position)
+    return 1
+
+
+def move_to_cursor(node):
+    """Moves input Node to position under cursor
+
+    Args:
+        node(hou.Node): Node to position
+
+    Returns 1 if Successful
+    """
+    pane = hou.ui.paneTabUnderCursor()
+    cursor_position = pane.cursorPosition()
+    node.setPosition(cursor_position)
+    return 1
